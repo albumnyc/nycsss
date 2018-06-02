6ffba6e26df83550a996d23eadcc23e34529f863
nieyichao <1609240765@qq.com>
2018-06-02 20:59:05 +0800

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+function init (){
+   var wid =  document.documentElement.getBoundingClientRect()['width'];
+    var num =  wid/15; //一rem对应25px
+    console.log(num);
+    document.documentElement.style.fontSize = `${num}px`;
+}
+window.onload = _=>{
+  init();
+}
+window.onresize = _=>{init()};
+
+export default {
+  name: 'App',
+  data(){
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './scss/base.scss';
+
+*{
+  font-size: pxtorem(14);
+  padding: 0px;
+  margin: 0px;
+  color: nth($color,5);
+}
+img{
+  width: percentage(1/10);
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
